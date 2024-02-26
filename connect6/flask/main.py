87ea0da68d56d1cecd169bb5cf4e448625acdd4e
wkcochran123 <wkcochran@gmail.com>
2024-02-26 10:21:58 -0500

    This is the attempt to build a classifier to understand if a board is part of a game or just randomly created

--- connect6/flask/main.py
+++ connect6/flask/main.py
@@ -1,6 +1,13 @@
+import random
+
 from flask import Flask, request
 app = Flask(__name__)
 
+def increment_side(side):
+    side = (side+1) % 5 
+    if side == 0:
+        side = side + 1
+    return side
 
 def make_new_board():
     board = []
@@ -29,12 +36,12 @@ def board_to_html_table(board,variables):
             local_variables = "i={}&j={}".format(i,j)
             ret = ret + "<td align=center valign=center>"
             if y == 0:
-                ret = ret + "<font size=+2><pre> <a href=/?"+variables+local_variables+ \
-                            " style='text-decoration: none;'>＋</a> </pre></font>"
+                ret = ret + "<font size=+3><pre><a href=/?"+variables+local_variables+ \
+                            " style='text-decoration: none;'>＋</a></pre></font>"
             elif y < 3:
-                ret = ret + "<font color=000000 size=+3><pre> ● \n</pre></font>"
+                ret = ret + "<font color=000000 size=+4><pre>●\n</pre></font>"
             else:
-                ret = ret + "<font color=FFFFFF size=+3><pre> ● \n</pre></font>"
+                ret = ret + "<font color=FFFFFF size=+4><pre>●\n</pre></font>"
             j = j + 1
             ret = ret + "\n \n</a></pre></td>"
         i = i + 1
@@ -42,30 +49,86 @@ def board_to_html_table(board,variables):
     ret = ret + "</table>"
     return ret
 
-def draw_board(board_string,i,j,side):
-    
-    board = []
-    if board_string == "":
-        board = make_new_board()
-    else:
-        board = parse_board(board_string)
-
-    board[i][j] = side
-
-
+def encode_board(board):
     bval = ""
     for i in board:
         for j in i:
             bval = "{}{}".format(bval,j)
+    return bval
 
-    side = (side+1) % 5 
-    if side == 0:
-        side = side + 1
+def draw_board(board,side,player):
+    
+    bval = encode_board(board)
 
     variables = "side={}&".format(side) +     \
-                "board={}&".format(bval)
+                "board={}&".format(bval) +    \
+                "player={}&".format(player)
 
     return board_to_html_table(board,variables)
+
+
+def get_header():
+    return """
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <title>Connect 6</title>
+    <style>
+       td {
+           width: 40px;
+           height: 50px;
+       }
+       a:link, a:visited {
+           color: #000000; /* Example color: blue */
+           text-decoration: none; /* Optional: removes underline from links */
+       }
+    </style>
+    </head>
+    """
+
+def compute_distance(stone_a, stone_b):
+    (x1,y1) = stone_a
+    (x2,y2) = stone_b
+    return abs(x2-x1) + abs(y1-y2)
+
+def compute_min_distance(stone,stones,threshold):
+    distance = 400 # This is far larger than the board
+    for other_stone in stones:
+        distance = min(distance, compute_distance(stone,other_stone))
+        if distance <= threshold:
+            return True
+    return False
+        
+
+def compute_move(board,side):
+    stones = []
+    i = 0
+    for x in board:
+        j = 0
+        for y in x:
+            if y != 0:
+                stones.append((i,j))
+        j = j + 1
+    i = i + 1
+
+    options = []
+    i = 0
+    for x in board:
+        j = 0
+        for y in x:
+            if y == 0:
+                if compute_min_distance((i,j),stones,2):
+                    options.append((i,j))
+        j = j + 1
+    i = i + 1
+
+    (i,j) = random.choice(options)
+    board[i][j] = side
+
+    side = increment_side(side)
+    return (board,side)
+    
+
 
 @app.route('/')
 def hello_world():
@@ -73,8 +136,32 @@ def hello_world():
     i = int(request.args.get("i","9"))
     j = int(request.args.get("j","9"))
     side = int(request.args.get("side","2"))
-    page = ""
-    page = page + draw_board(board,i,j,side)
+    t = random.choice(["b","w"])
+    print(t)
+    player = request.args.get("player",t)
+    print(player)
+
+    board_list = []
+    if "board" in request.args:
+        board_list = parse_board(board)
+    else:
+        board_list = make_new_board()
+
+    board_list[i][j] = side
+    side = increment_side(side)
+
+    if (player == "w" and side == 1) or (player == "b" and side == 3):
+        (board_list,side) = compute_move(board_list,side)
+        (board_list,side) = compute_move(board_list,side)
+
+    page = get_header() + "<body>"
+
+    if player == "b":
+        page = page + "<h2>You are BLACK</h2>"
+    else:
+        page = page + "<h2>You are WHITE</h2>"
+
+    page = page + draw_board(board_list,side,player) + "</body></html>"
     return 'Hello, World!<br>' + page + "<br><a href=/>Restart</a>"
 
 if __name__ == '__main__':
